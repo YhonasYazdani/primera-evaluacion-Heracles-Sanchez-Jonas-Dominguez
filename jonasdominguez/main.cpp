9ab3376c4d993a98b35648ccfbeaa6af5de0ed29
YhonasYazdani <131405673+YhonasYazdani@users.noreply.github.com>
2024-06-26 09:41:17 -0400

    Primer subida
    Datos de entrada y calculo segun su cuota

---
+++ jonasdominguez/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main ( int**argc, string** , float**, double** ) {
+	return 0;
+	rzs string;..;
+	cliente string:..;
+	cedula_rif float;..;
+	cp_my double;..;
+	cp_dt double;..;
+	motpg float;..;
+	refrescos float;..;
+	maltas float;..;
+	refrescos2 float;..;
+	malta2 float;..;
+	cn_p float;..;
+}
+int cp_my: >5
+int cp_dt: <6
+ else cn_p= cp_dt
+int refrescos: cn_p * cuota_prf
+int refrescos1: 10$
+int refrescos2: 5$
+int maltas1: 12$
+int maltas2: 7$
+int cuota_prf: if cn_p= cp_my
+std :: maltas2 * 
+int motpg:  refrecos * cn_p
+int cdg: if 001= refrescos if 002= maltas
+std :: cout " ingrese larazon social: " ;
+std :: cin >> rzs
+std :: cout " coloque la cedula o rif: " ;
+std :: cin >> cedula_rif
+std :: cout "cantidad de productos al comprar: " ;
+std :: cin >> cn_p
+std :: cout "Ingrese el tipo de producto" ;
+std :: cin >> cdg
+stp :: cout ""
+
+
